a6038d7a69f200395766f4b143a3173e42f9db2c
lucho20091 <124323772+lucho20091@users.noreply.github.com>
2025-10-18 20:47:58 -0500

    ye

--- app/sign-in/page.jsx
+++ app/sign-in/page.jsx
@@ -5,9 +5,9 @@ import { redirect } from "next/navigation";
 
 export default async function SignInPage() {
   const user = await stackServerApp.getUser();
-  //   if (user) {
-  //     redirect("/dashboard");
-  //   }
+  if (user) {
+    redirect("/dashboard");
+  }
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-50 to-purple-100">
       <div className="max-w-md w-full space-y-8">
